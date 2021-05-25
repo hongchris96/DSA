14d96ece108267af7a230c8bfad13d6ea5dee22c
Chris Hanma <hongchris96@hotmail.com>
2021-05-25 16:17:35 -0700

    complete quicksort

--- quick_sort_project/lib/quick_sort.js
+++ quick_sort_project/lib/quick_sort.js
@@ -1,5 +1,12 @@
 function quickSort(array) {
+    if (array.length <= 1) return array;
 
+    let pivot = array.shift();
+    let left = quickSort(array.filter(ele => ele < pivot));
+    let right = quickSort(array.filter(ele => ele >= pivot));
+
+    left.push(pivot, ...right);
+    return left;
 }
 
 
