73ecacd24b36332f12e05bf0c1148cfc7737e6c9
Chris Hanma <hongchris96@hotmail.com>
2021-05-25 16:06:19 -0700

    Complete selection and insertion sort

--- insertion_sort_project/lib/insertion_sort.js
+++ insertion_sort_project/lib/insertion_sort.js
@@ -1,5 +1,12 @@
 function insertionSort(arr) {
-
+    for (let i = 1; i < arr.length; i++) {
+        let currentElement = arr[i];
+        for (var j = i - 1; j >= 0 && currentElement < arr[j]; j--) {
+            arr[j + 1] = arr[j];
+        }
+        arr[j + 1] = currentElement;
+    }
+    return arr;
 }
 
 module.exports = {
