ddfff677d32d48b005bf740632a50c2be845235a
Chris Hanma <hongchris96@hotmail.com>
2021-05-25 16:06:44 -0700

    Update selection sort

--- selection_sort_project/lib/selection_sort.js
+++ selection_sort_project/lib/selection_sort.js
@@ -1,9 +1,16 @@
 function swap(arr, index1, index2) {
-
+    [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
+    return arr;
 }
 
 function selectionSort(arr) {
-
+    for (let i = 0; i < arr.length - 1; i++) {
+        let currentMin = arr[i];
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < currentMin) swap(arr, i, j);
+        }
+    }
+    return arr;
 }
 
 module.exports = {
