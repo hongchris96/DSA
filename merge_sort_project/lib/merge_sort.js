5b60a83acfb533127b0e8d07e1f9fe267655ec9c
Chris Hanma <hongchris96@hotmail.com>
2021-05-25 16:28:36 -0700

    Complete mergesort

--- merge_sort_project/lib/merge_sort.js
+++ merge_sort_project/lib/merge_sort.js
@@ -1,9 +1,21 @@
 function merge(array1, array2) {
-
+    let newArr = [];
+    while (array1.length !== 0 && array2.length !== 0) {
+        if (array1[0] < array2[0]) newArr.push(array1.shift());
+        else newArr.push(array2.shift());
+    }
+    newArr.push(...array1, ...array2);
+    return newArr;
 }
 
 function mergeSort(array) {
+    if (array.length <= 1) return array;
 
+    let middle = Math.floor(array.length / 2);
+    let left = mergeSort(array.slice(0, middle));
+    let right = mergeSort(array.slice(middle));
+
+    return merge(left, right);
 }
 
 module.exports = {
