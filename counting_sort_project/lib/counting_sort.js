15557b58af349e1ef39eb8f89a3596a535e35b09
Chris Hanma <hongchris96@hotmail.com>
2021-05-26 17:04:02 -0700

    Complete counting sort

--- counting_sort_project/lib/counting_sort.js
+++ counting_sort_project/lib/counting_sort.js
@@ -1,5 +1,9 @@
 function countingSort(arr, max) {
-
+    let buckets = new Array(max+1).fill().map(() => new Array);
+    for (let i = 0; i < arr.length; i++) {
+        buckets[arr[i]].push(arr[i]);
+    }
+    return [].concat(...buckets);
 }
 
 
