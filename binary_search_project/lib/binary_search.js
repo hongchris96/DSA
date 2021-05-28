40e139fe742ac63ecfa28d91d16a998a5ac84bb0
Chris Hanma <hongchris96@hotmail.com>
2021-05-28 16:10:22 -0700

    Complete Binary Search practice

--- binary_search_project/lib/binary_search.js
+++ binary_search_project/lib/binary_search.js
@@ -1,9 +1,25 @@
 function binarySearch(array, target) {
-
+    // console.log(array);
+    if (array.length === 0) return false;
+    
+    let midIdx = Math.floor(array.length / 2);
+    if (array[midIdx] === target) return true;
+    else if (array[midIdx] > target) return binarySearch(array.slice(0, midIdx), target);
+    else {
+        return binarySearch(array.slice(midIdx + 1), target);
+    }
 }
 
 function binarySearchIndex(array, target) {
+    if (array.length === 0) return -1;
 
+    let midIdx = Math.floor(array.length / 2);
+    if (array[midIdx] === target) return midIdx;
+    else if (array[midIdx] > target) return binarySearchIndex(array.slice(0, midIdx), target);
+    else {
+        let result = binarySearchIndex(array.slice(midIdx + 1), target);
+        return result === -1 ? -1 : midIdx + result + 1;
+    }
 }
 
 
@@ -11,3 +27,6 @@ module.exports = {
     binarySearch,
     binarySearchIndex
 };
+
+
+// console.log(binarySearch([5, 10, 12, 15, 20, 30, 70], 24));
