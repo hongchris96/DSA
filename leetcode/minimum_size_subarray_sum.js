24b9e84daa83d976ba52551172a73714ba5abe53
Chris Hanma <hongchris96@hotmail.com>
2021-06-02 12:04:36 -0700

    Attempt leetcode 209 and 162

---
+++ leetcode/minimum_size_subarray_sum.js
@@ -0,0 +1,45 @@
+// 209. Minimum Size Subarray Sum
+// Medium
+
+// Given an array of positive integers nums and a positive integer target, return the minimal length of a contiguous subarray [numsl, numsl+1, ..., numsr-1, numsr] of which the sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+// Example 1:
+// Input: target = 7, nums = [2,3,1,2,4,3]
+// Output: 2
+// Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+
+// Example 2:
+// Input: target = 4, nums = [1,4,4]
+// Output: 1
+
+// Example 3:
+// Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+// Output: 0
+ 
+// Constraints:
+// 1 <= target <= 109
+// 1 <= nums.length <= 105
+// 1 <= nums[i] <= 105
+
+var minSubArrayLen = function(target, nums) {
+    let minLength = Infinity;
+    let currentSum = 0;
+    let currentLength = 1;
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) return 1;
+        for (let j = i + 1; j < nums.length || j < i + minLength; j++) {
+            currentSum += nums[j];
+            currentLength += 1;
+            if (currentSum >= target) {
+                if (currentLength < minLength) {
+                    minLength = currentLength;
+                    currentSum = 0;
+                }
+                break;
+            }
+        }
+    }
+    return minLength;
+};
+
