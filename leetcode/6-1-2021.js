03da7aa945d70bafff95feaa2bcdbc95939ca469
Chris Hanma <hongchris96@hotmail.com>
2021-06-01 20:23:45 -0700

    Complete Shuffle String

--- leetcode/6-1-2021.js
+++ leetcode/6-1-2021.js
@@ -152,4 +152,60 @@ var smallerNumbersThanCurrent = function(nums) {
     return answer;
 };
 
+// 1528. Shuffle String
+
+// Given a string s and an integer array indices of the same length.
+// The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+// Return the shuffled string.
+
+// Example 1:
+
+
+// Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+// Output: "leetcode"
+// Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+// Example 2:
+
+// Input: s = "abc", indices = [0,1,2]
+// Output: "abc"
+// Explanation: After shuffling, each character remains in its position.
+// Example 3:
+
+// Input: s = "aiohn", indices = [3,1,4,2,0]
+// Output: "nihao"
+// Example 4:
+
+// Input: s = "aaiougrt", indices = [4,0,2,6,7,3,1,5]
+// Output: "arigatou"
+// Example 5:
+
+// Input: s = "art", indices = [1,0,2]
+// Output: "rat"
+
+
+var restoreString = function(s, indices) {
+    let answer = [];
+    for (let i = 0; i < s.length; i++) {
+        answer[indices[i]] = s[i];
+    }
+    
+    return answer.join('');
+    
+    // let letterIndices = [];
+    // for (let i = 0; i < s.length; i++) {
+    //     letterIndices.push([s[i], indices[i]]);
+    // }
+    // return qs(letterIndices).map(ele => ele[0]).join('');
+};
+
+const qs = function(arr) {
+    if (arr.length <= 1) return arr;
+    
+    let piv = arr[0];
+    
+    let left = arr.slice(1).filter((ele) => ele[1] < piv[1]);
+    let right = arr.slice(1).filter((ele) => ele[1] >= piv[1]);
+    
+    return qs(left).concat([piv], qs(right));
+}
 
