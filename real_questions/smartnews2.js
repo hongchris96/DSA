df8cd89f9b96eef3392f07256bc164dc1e0b4f38
Chris Hanma <hongchris96@hotmail.com>
2021-06-03 16:57:28 -0700

    Solved smartnews2 brute force

--- real_questions/smartnews2.js
+++ real_questions/smartnews2.js
@@ -21,38 +21,30 @@ function checkSwitch(arr) {
 
 function solution(A) {
     // write your code in JavaScript (Node.js 8.9.4)
-    let start = 0;
-    let ending = A.length - 1;
+
     let longest = 2;
 
-    while (start < A.length - 1) {
-        let currentSlice = A.slice(start);
-        // console.log(currentSlice);
-        if (checkSwitch(currentSlice)) {
-            if (currentSlice.length > longest) {
-                longest = currentSlice.length;
+    for (let start = 0; start < A.length - 2; start++) {
+        let currentLongest = 2;
+        for (let ending = A.length; ending > start + 2; ending--) {
+            let currentSlice = A.slice(start, ending);
+            if (checkSwitch(currentSlice)) {
+                currentLongest = currentSlice.length;
+                if (currentLongest > longest) {
+                    longest = currentLongest;
+                }
+                break;
             }
-            break;
-        } else {
-            start += 1;
         }
     }
-
-    while (ending > 1) {
-        let currentSlice = A.slice(0, ending + 1);
-        console.log(currentSlice);
-        if (checkSwitch(currentSlice)) {
-            if (currentSlice.length > longest) {
-                longest = currentSlice.length;
-            }
-            break;
-        } else {
-            ending -= 1;
-        }
-    }
-
+    
     return longest;
 }
 
-console.log(solution([7, 4, -2, 4, -2, -9]));
+console.log(solution([7, 4, -2, 4, -2, -9])); // 4
+console.log(solution([7, 7, 7, 4, -2, 4, -2, -9])); // 4
+console.log(solution([7, 4, -2, 4, -9])); // 3
+console.log(solution([7, 4, -2, 4, -2, -9, -2, -9, -2])); // 5
+console.log(solution([1, 2, 3, 4, 5])) // 2
+console.log(solution([3, 3, 3, 4, 5])) // 3
 
