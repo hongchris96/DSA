1fa29608ddb41be611dd126c911da0b26863d021
Chris Hanma <hongchris96@hotmail.com>
2021-07-09 12:17:51 -0700

    Annotate all paths from source to target

--- leetcode/797_all_path.js
+++ leetcode/797_all_path.js
@@ -9,17 +9,26 @@
 // Explanation: There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
 
 var allPathsSourceTarget = function(graph) {
+  // Store all paths
   let answer = [];
-  let endVal = graph.length - 1
+  // End of the path value
+  let endVal = graph.length - 1;
+  // Build all possible path, starting at node value of 0
+  // also pass in overall graph, total path storage, end point
   buildPath([0], graph, answer, endVal);
   return answer;
 };
 
-var buildPath = function(number, graph, total, endVal) {
-  let lastNumber = number[number.length - 1];
-  if (lastNumber === endVal) return number;
+var buildPath = function(path, graph, total, endVal) {
+  // the last number of the current path
+  let lastNumber = path[path.length - 1];
+  // last number is the end point, path is complete 
+  if (lastNumber === endVal) return path;
+  // continue building path from the last number
   for (let i = 0; i < graph[lastNumber].length; i++) {
-      let subPath = buildPath(number.concat([graph[lastNumber][i]]), graph, total, endVal);
-      if (subPath) total.push(subPath);
+    // building subpath recursively with current path so far (path + a number in the subarray)
+    let subPath = buildPath(path.concat([graph[lastNumber][i]]), graph, total, endVal);
+    // if subPath is not undefined
+    if (subPath) total.push(subPath);
   }
 }
