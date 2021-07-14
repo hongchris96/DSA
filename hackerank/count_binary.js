2a18db1677a3eaa047fdeaaceb1f3928e1c37342
Chris Hanma <hongchris96@hotmail.com>
2021-07-14 16:52:06 -0700

    Modify count binary substring to improve runtime

--- hackerank/count_binary.js
+++ hackerank/count_binary.js
@@ -10,28 +10,56 @@ function getSubstringCount(s) {
   // keep track of 0 count and 1 count
   // when n !== n-1 and 0 count === 1 count, answer += 1
   
+  // let answer = 0;
+  // for (let i = 0; i < s.length - 1; i++) {
+  //     for (let j = i + 1; j < s.length; j++) {
+  //         if (isEven(s.slice(i, j+1))) {
+  //             console.log(s.slice(i, j+1));
+  //             answer += 1;
+  //         }
+  //     }
+  // }
+  // return answer;
+
+  // Improve runtime maybe?
   let answer = 0;
-  for (let i = 0; i < s.length - 1; i++) {
-      for (let j = i + 1; j < s.length; j++) {
-          if (isEven(s.slice(i, j+1))) {
-              console.log(s.slice(i, j+1));
-              answer += 1;
-          }
+  let groups = [];
+  let i = 1;
+  let currentType = s[0];
+  let currentGroupCount = 1;
+  while ( i < s.length) {
+    if (s[i] === currentType) {
+      currentGroupCount += 1;
+      if (i === s.length - 1) {
+        groups.push(currentGroupCount);
       }
+    } else {
+      groups.push(currentGroupCount);
+      currentType = s[i];
+      currentGroupCount = 1;
+    }
+    i++;
+  }
+  for (let i = 0; i < groups.length - 1; i++) {
+    if (groups[i] === groups[i+1]) {
+      answer += groups[i];
+    } else {
+      answer += Math.min(groups[i+1], groups[i]);
+    }
   }
   return answer;
 }
 
-function isEven(s) {
-  if (s.length % 2) return false;
-  let splitIdx = [];
-  for (let i = 1; i < s.length; i++) {
-      if (s[i] !== s[i-1]) {
-          splitIdx.push(i);
-      }
-  }
-  if (!splitIdx.length || splitIdx.length > 1) return false;
-  return s.slice(0, splitIdx).length === s.slice(splitIdx).length;
-}
+// function isEven(s) {
+//   if (s.length % 2) return false;
+//   let splitIdx = [];
+//   for (let i = 1; i < s.length; i++) {
+//       if (s[i] !== s[i-1]) {
+//           splitIdx.push(i);
+//       }
+//   }
+//   if (!splitIdx.length || splitIdx.length > 1) return false;
+//   return s.slice(0, splitIdx).length === s.slice(splitIdx).length;
+// }
 
 console.log(getSubstringCount("001100011")); // should return 6
