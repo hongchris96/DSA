715f6c6277075d321e71b5b981819dcbe16231b9
Chris Hanma <hongchris96@hotmail.com>
2021-07-27 10:45:19 -0700

    Attempt array game

---
+++ hackerank/array_game.js
@@ -0,0 +1,21 @@
+// Number of moves to make all elements equal in array
+// Each move increments 4 out of 5 elements in the array by 1
+
+function countMoves(numbers) {
+  // Write your code here
+  let maxNum = Math.max(...numbers);
+  let moves = 0;
+  while(!numbers.every(ele => ele === maxNum)) {
+      let maxEncountered = false;
+      for (let i = 0; i < numbers.length; i++) {
+          if (numbers[i] !== maxNum || maxEncountered) {
+              numbers[i] += 1;
+          } else {
+              maxEncountered = true;
+          }
+      }
+      moves += 1;
+      maxNum = Math.max(...numbers);
+  }
+  return moves;
+}
