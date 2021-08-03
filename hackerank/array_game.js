746ca618041e63d82a5289d1d4fbe265554405e4
Chris Hanma <hongchris96@hotmail.com>
2021-08-02 20:52:22 -0700

    Update array game to optimize run time

--- hackerank/array_game.js
+++ hackerank/array_game.js
@@ -4,19 +4,31 @@
 // Run Time failed for large input
 function countMoves(numbers) {
   // Write your code here
-  let maxNum = Math.max(...numbers);
+  // let maxNum = Math.max(...numbers);
+  // let moves = 0;
+  // while(!numbers.every(ele => ele === maxNum)) {
+  //     let maxEncountered = false;
+  //     for (let i = 0; i < numbers.length; i++) {
+  //         if (numbers[i] !== maxNum || maxEncountered) {
+  //             numbers[i] += 1;
+  //         } else {
+  //             maxEncountered = true;
+  //         }
+  //     }
+  //     moves += 1;
+  //     maxNum = Math.max(...numbers);
+  // }
+  // return moves;
+
+  // ------------------------------------------------------------------------------------
+  // Solution: Instead of incrementing all 4 elements, look at how decrement 1 element
+
+  let minNum = Math.min(...numbers);
   let moves = 0;
-  while(!numbers.every(ele => ele === maxNum)) {
-      let maxEncountered = false;
-      for (let i = 0; i < numbers.length; i++) {
-          if (numbers[i] !== maxNum || maxEncountered) {
-              numbers[i] += 1;
-          } else {
-              maxEncountered = true;
-          }
-      }
-      moves += 1;
-      maxNum = Math.max(...numbers);
+  for (let num of numbers) {
+    if (num !== minNum) {
+      moves += num - minNum;
+    }
   }
   return moves;
 }
