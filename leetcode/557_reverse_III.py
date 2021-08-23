503f7b665a101757aea2170cc699f7910ede4795
Chris Hanma <hongchris96@hotmail.com>
2021-08-23 08:59:50 -0700

    Warmup reverse words in string III

---
+++ leetcode/557_reverse_III.py
@@ -0,0 +1,18 @@
+# 557. Reverse Words in a String III
+
+# Given a string s, reverse the order of characters in each word within a
+# sentence while still preserving whitespace and initial word order.
+
+# Example 1:
+# Input: s = "Let's take LeetCode contest"
+# Output: "s'teL ekat edoCteeL tsetnoc"
+
+# Example 2:
+# Input: s = "God Ding"
+# Output: "doG gniD"
+
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        arr = [word[::-1] for word in s.split(" ")]
+        return " ".join(arr)
